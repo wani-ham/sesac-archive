7ac87193a17813df8b67bfbfc3aeb6ab5c95cffd
wani-ham <neoham7@gmail.com>
2024-05-31 16:15:54 +0900

    0531 lecture: 04_js update and backup

--- 04_js/index7.js
+++ 04_js/index7.js
@@ -38,10 +38,48 @@
 // let now = new Date().getHours();
 // now >= 12 ? console.log("Good evening!") : console.log("Good morning!");
 
+// let sum = 0;
+// for(let i = 1;
+//     i <= 20;
+//     i%2 === 0 ? sum += i: false,
+//     i++
+// );
+// console.log(sum);
+
+// let i = 2, j = 1;
+// while (i < 10) {
+//     while (j < 10) {
+//         console.log(`KukuDan!: ${i} * ${j} = ${i*(j++)}`);
+//     }
+//     i++;
+//     j = 1;
+// }
+
+// for (let i = 1; i < 10; i++){
+//     for(let j = 1; 
+//         j < 10; 
+//         console.log(`Kuku: ${i} x ${j} = ${i*(j++)}`)
+//     );
+// }
+
+// do while example
+// do {
+    // number = parseInt(prompt("Enter num bigger than 10: "));
+// } while (number <= 10);
+
+
+// let n = Number(prompt("Enter num: "));
+// let n = 10000;
+// for(let i = 1; 
+//     i <= n; 
+//     !(i%13) ? console.log(i) : false,
+//     i += 2
+// );
+
 let sum = 0;
-for(let i = 1;
-    i <= 20;
-    i%2 === 0 ? sum += i: false,
-    i++
-);
+for(let i = 0; i <= 100; i++) {
+    if(!(i%2) || !(i%5)) {
+        sum += i;
+    }
+}
 console.log(sum);

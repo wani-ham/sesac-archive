e84dfbe5ebbc26359467781a6514d73a001c97c9
wani-ham <neoham7@gmail.com>
2024-05-31 15:07:18 +0900

    0531 lecture: 04_js update and backup

--- 04_js/index7.js
+++ 04_js/index7.js
@@ -15,21 +15,33 @@
 
 // 실습
 
-let score = parseInt(prompt("Enter your score: "));
-switch (true) {
-    case score >= 90:
-        console.log("A");
-        break;
-    case score >= 80:
-        console.log("B");
-        break;
-    case score >= 70:
-        console.log("C");
-        break;
-    case score >= 60:
-        console.log("D");
-        break;
-    default:
-        console.log("F");
-        break;
-}
+// let score = parseInt(prompt("Enter your score: "));
+// switch (true) {
+//     case score >= 90:
+//         console.log("A");
+//         break;
+//     case score >= 80:
+//         console.log("B");
+//         break;
+//     case score >= 70:
+//         console.log("C");
+//         break;
+//     case score >= 60:
+//         console.log("D");
+//         break;
+//     default:
+//         console.log("F");
+//         break;
+// }
+
+
+// let now = new Date().getHours();
+// now >= 12 ? console.log("Good evening!") : console.log("Good morning!");
+
+let sum = 0;
+for(let i = 1;
+    i <= 20;
+    i%2 === 0 ? sum += i: false,
+    i++
+);
+console.log(sum);

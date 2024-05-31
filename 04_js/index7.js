ab83a87c8da5eb0ab533e3772b3af0a64d1b1cbe
wani-ham <neoham7@gmail.com>
2024-05-31 13:33:17 +0900

    0531 lecture: 04_js update and backup

--- 04_js/index7.js
+++ 04_js/index7.js
@@ -1,15 +1,35 @@
 // Javascript Statement
 
-if (3 < 5) {
-    console.log("True");
-}
+// if (3 < 5) {
+//     console.log("True");
+// }
 
-let number = Number(prompt("Enter Number: "));
-if (number > 10) {
-    console.log("Bigger than 10");
-}
-else {
-    console.log("Smaller than 10");
-}
+// let number = Number(prompt("Enter Number: "));
+// if (number > 10) {
+//     console.log("Bigger than 10");
+// }
+// else {
+//     console.log("Smaller than 10");
+// }
 
 
+// 실습
+
+let score = parseInt(prompt("Enter your score: "));
+switch (true) {
+    case score >= 90:
+        console.log("A");
+        break;
+    case score >= 80:
+        console.log("B");
+        break;
+    case score >= 70:
+        console.log("C");
+        break;
+    case score >= 60:
+        console.log("D");
+        break;
+    default:
+        console.log("F");
+        break;
+}

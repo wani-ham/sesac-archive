386ac49fbb66cefdd51114105ca8e22127219e91
wani-ham <neoham7@gmail.com>
2024-06-03 16:38:27 +0900

    0603 lecture: JS update

--- 04_js_dom/index4.js
+++ 04_js_dom/index4.js
@@ -34,7 +34,7 @@ for(let li of list) {
 // xxx.classList.contains : 있는지 없는지 확인 (t/f)
 // xxx.classList.toggle : 있으면 제거, 없으면 추가
 
-// h1.classList.add('add-h1');
+h1.classList.add('add-h1');
 h1.classList.remove('add-h1');
 console.log(h1.classList.contains('add-h1'));
 h1.classList.toggle('add-h1')
@@ -43,26 +43,74 @@ console.log('-------------------------');
 // 요소 찾기
 // 계층 구조 (형제, 자식, 부모, 조상, 자손)
 const friends = document.querySelector('#friends');
-const tigger = document.querySelector('#tigger');
+const kevin = document.querySelector('#kevin');
 
 // 1. 자식 요소
 console.log(friends.children); // 유사 배열, 자식 모두 선택
 console.log(friends.children[0]); // 인덱스 접근.
 
 // 2. 부모 요소
-console.log(tigger.parentNode);
-console.log(tigger.parentNode.parentNode);
+console.log(kevin.parentNode);
+console.log(kevin.parentNode.parentNode);
 
 // 3. 형제 요소
-console.log(tigger.previousElementSibling); // 이전
-console.log(tigger.nextElementSibling); // 다음
-console.log(tigger.nextElementSibling.nextElementSibling);
+console.log(kevin.previousElementSibling); // 이전
+console.log(kevin.nextElementSibling); // 다음
+console.log(kevin.nextElementSibling.nextElementSibling);
 
 // 실습 1
+console.log("============================");
 const taste = document.querySelector('div > span');
 taste.innerText = "맛없다 ㅡㅡ;;";
 taste.style.fontSize = 'larger';
 taste.style.fontWeight = 'bold';
 taste.style.color = 'red';
+console.log("============================");
 
+
+
+// // 새로운 요소 생성
+// const container = document.querySelector('.container');
+// const p = document.createElement('p'); // 요소 생성
+// console.log(p); // js로 <p></p> 태그를 만듦.
+// p.innerText = '새로 추가된 p 요소 입니다~;' 
+// p.style.fontWeight = 700;
+// p.style.backgroundColor ='red';
+
+// // 새로 만든 요소를 추가해보자!
+// // x.append(y): x 요소의 맨 마지막 자식으로 y 요소가 추가
+// // x.appendChild(y)
+// div1.appendChild(p);
+
+// const p2 = document.createElement('p');
+// const p3 = document.createElement('p');
+
+// p2.innerHTML = 'p2';
+// p3.innerHTML = 'p3';
+
+// p2.classList.add('p-2');
+// p3.classList.add('p-3');
+
+// container.append(p2, p3); // 여러개 추가 가능!
+
+// // x.prepend(y): x요소의 맨 처음 자식으로 y 요소가 추가
+// const li1 = document.createElement('li');
+// li1.textContent = "캉가";
+// friends.prepend(li1);
+
+// const li0 = document.createElement('li');
+// li0.innerHTML = '<b>친구들을 소개합니다</b>';
+// friends.prepend(li0);
+
+// // 요소 삭제
+// // x.remove(): x 요소 자체를 삭제
+// // x.removeChild(y) : x의 자식요소인 y가 삭제
+// const firstLi = document.querySelector('li');
+// console.log(firstLi); // 친구들을 소개합니다 li 태그
+
+// const ul = firstLi.parentNode;
+// // ul.removeChild(firstLi);
+
+// firstLi.remove();
+// ul.remove();
 

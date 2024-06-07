812c65e649ec8d5f4d3cf4cde69dc0b4f54e03a0
wani-ham <neoham7@gmail.com>
2024-06-07 18:22:54 +0900

    0607 lecture: update 06_node/http_test

--- 06_node/http_test/server.js
+++ 06_node/http_test/server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
-    fs.readFile('index.html', (err, data) => {
+    fs.readFile('./views/index.html', (err, data) => {
         if(err) {
             console.log(err);
             res.end();
---
+++ 06_node/http_test/server2.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+    let path = "./views/";
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        default:
+            res.statusCode = 404;
+            break;
+
+    }
+
+    fs.readFile(path, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end();
+        }
+        else {
+            res.end(data);
+        }
+    });
+});
+
+server.listen(8080, () => {
+    console.log('8080 port running...');
+})

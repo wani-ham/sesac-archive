dc880f33af05fb533ef53e12928cb9a4dd3c26aa
wani-ham <neoham7@gmail.com>
2024-06-14 13:35:46 +0900

    0614 lecture: update 09_async

---
+++ 09_async/index.js
@@ -0,0 +1,34 @@
+console.log(1);
+// setTimeout(code, delay)
+// wait for delay and execute code
+/*
+setTimeout(() => {
+    console.log(2);
+}, 2000);
+console.log(3);
+*/
+// result: 1 -> 3 -> 2
+// js 에서는 setTimeout 함수를 사용했을 때, 기다리지 않고 
+// 바로 다음 구문을 실행함.
+
+function goMart() {
+    console.log("What drink should I buy at the mart!");
+}
+
+function pickDrink() {
+    setTimeout(() => {
+        console.log("Aha!");
+        product = "Diet Coke";
+        price = 2000;
+    }, 3000);
+}
+
+function pay(product, price) {
+    console.log(`Name: ${product}, Price: ${price}`);
+}
+
+let product;
+let price;
+goMart();
+pickDrink();
+pay(product, price);

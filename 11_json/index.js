0adf67c690943607a0c487500d6276c2f765cd81
wani-ham <neoham7@gmail.com>
2024-06-17 14:39:10 +0900

    0617 lecture: 10_form update

---
+++ 11_json/index.js
@@ -0,0 +1,16 @@
+// JSON
+const car = ` {
+    "model": "IONIQ 5",
+    "company": "Hyundai",
+    "price": 5000,
+    "year": 2024,
+    "isElectricCar": true,
+    "options": ["side mirror", "smart sensor", "built-in cam"]
+}`;
+
+console.log(car); // type: json
+
+// Deserialize
+// json to js obj
+const obj = JSON.parse(car);
+console.log(obj);

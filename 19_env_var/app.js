0544a9e131c9cbdfee43e505227ef9d2c051f1cc
wani-ham <neoham7@gmail.com>
2024-07-01 14:23:02 +0900

    0701: update 19_env_var

--- 19_env_var/app.js
+++ 19_env_var/app.js
@@ -3,7 +3,17 @@
 
 const express = require('express');
 const app = express();
-require('dotenv').config();
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({
+    path: path.resolve(__dirname, '.env')
+})  // default .env file
+dotenv.config({
+    // NODE_ENV 따라서 적절한 .env 파일을 로드
+    path: path.resolve(__dirname, `.env.${process.env.NODE_ENV}`),
+    override: true,
+});  // load env file depending on NODE_ENV
 
 const port = process.env.PORT;
 const dbName = process.env.DATABASE_NAME;
@@ -13,4 +23,3 @@ app.listen(port, () => {
     console.log(`Server running...PORT: ${port}`);
     console.log(`Database name: ${dbName}, Database Password: ${dbPw}`);
 })
-

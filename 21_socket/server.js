83f2fec9da969224b9efbf19d864d389e404c332
wani-ham <neoham7@gmail.com>
2024-07-03 18:55:25 +0900

    0703: update 21_socket

--- 21_socket/server.js
+++ 21_socket/server.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -20,6 +21,10 @@ app.get('/', (req, res) => {
     res.render('client');
 })
 
+// 실습 3-2-1
+const nickObjs = {};
+
+
 // io.on(): socket 관련한 통신 작업을 처리
 io.on('connection', (socket) => {
     // connection event occurs when client is connected
@@ -27,21 +32,37 @@ io.on('connection', (socket) => {
     // socket.id: socket 고유 id (browser tab 단위)
 
     // 실습 1
-    const socketLog = (data, addStr) => {
-        console.log(`Who: ${data.who}, MSG: ${data.msg}`);
-        socket.emit(`${addStr}`, {who: 'server', msg: 'Hello Client! from ' + addStr});
-    }
+    // const socketLog = (data, addStr) => {
+    //     console.log(`Who: ${data.who}, MSG: ${data.msg}`);
+    //     socket.emit(`${addStr}`, {who: 'server', msg: 'Hello Client! from ' + addStr});
+    // }
 
-    socket.on('hello', (data) => {
-        socketLog(data, 'hellokr');
-    })
+    // socket.on('hello', (data) => {
+    //     socketLog(data, 'hellokr');
+    // })
 
-    socket.on('study', (data) => {
-        socketLog(data, 'studykr');
-    })
+    // socket.on('study', (data) => {
+    //     socketLog(data, 'studykr');
+    // })
 
-    socket.on('bye', (data) => {
-        socketLog(data, 'byekr');
+    // socket.on('bye', (data) => {
+    //     socketLog(data, 'byekr');
+    // })
+
+    // 실습 3
+    io.emit('notice', `${socket.id} entered the room.`);
+
+    // 실습 3-2
+    // emit() from server
+    //   - socket.emit(event_name, data) : 해당 클라이언트에게만 이벤트, 데이터를 전송
+    //   - io.emit(event_name, data) : 서버에 접속된 모든 클라이언트에게 전송
+    //   - io.to(socketid).emit(event_name, data) : 소켓아이디에 해당되
+
+    socket.on('setNick', (nick) => {
+        console.log(`${nick} entered`);
+
+        // 실습 3-2-1
+        // front에서 입력한 nick이 nickObjs
     })
 })
 

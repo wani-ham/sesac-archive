9ce4d01146f0cb401bec8953343def41ac7cd8de
wani-ham <neoham7@gmail.com>
2024-07-29 19:00:31 +0900

    0729 lecture: make dir 23 24

--- 24_props/24-props/src/button.js
+++ 24_props/24-props/src/button.js
@@ -1,5 +1,8 @@
 import './button.css'
 import PropTypes from 'prop-types'
+// props.children
+// : 부모 컴포넌트에서 자식 컴포넌트 호출 시 태그 사이에 작성한 문자열
+// 문자열, 숫자, JSX, 배열 등 다양한 형태.
 
 const Button = (props) => {
     const {link, children} = props;

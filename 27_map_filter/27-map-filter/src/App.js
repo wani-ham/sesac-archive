04b7077eb27f5913943f4b2d9f0a14741b66d2eb
wani-ham <neoham7@gmail.com>
2024-08-07 15:58:44 +0900

    0807: lecture

---
+++ 27_map_filter/27-map-filter/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Base from './Base';
+import Alphabet from './Alphabet';
+
+function App() {
+  return (
+    <div className="App">
+      <Base />
+      <Alphabet />
+    </div>
+  );
+}
+
+export default App;

e8b32747accfdec6e6ee1b1ed3b0cd93402d282e
wani-ham <neoham7@gmail.com>
2024-08-12 17:57:31 +0900

    0812: lecture

---
+++ 30_styles/30-styles/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import CssModuleComponets from './CssModuleComponents';
+import SassComponent from './SaasComponent';
+import StyledComponent from './StyledComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <CssModuleComponets /> <br />
+      <SassComponent /> <br />
+      <StyledComponent /> <br />
+    </div>
+  );
+}
+
+export default App;
